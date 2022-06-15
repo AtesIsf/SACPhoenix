92662f76869ae8b7f108ec5531e117ba5eb8f13f
AtesIsf <isfendiyar510@gmail.com>
2022-06-15 14:59:59 +0300

    Final

--- model.py
+++ model.py
@@ -3,20 +3,23 @@ from keras.layers import Dense, Dropout
 
 ### Creating the model ###
 
-ARCHITECTURE = "1"
+ARCHITECTURE = "3"
 
 model = keras.Sequential()
 model.add(keras.layers.Input(shape=(17,))) # Do not change the shape
 
 # Tweak the layers in between
-model.add(Dense(1000, activation="sigmoid"))
+model.add(Dense(128))
+model.add(Dropout(0.2))
 
-model.add(Dense(1000, activation="sigmoid"))
+model.add(Dense(64))
+model.add(Dropout(0.2))
 
-model.add(Dense(200, activation="sigmoid"))
+model.add(Dense(32))
+model.add(Dropout(0.2))
 
-model.add(Dense(200, activation="sigmoid"))
+model.add(Dense(16))
+model.add(Dropout(0.2))
 
 
-
-model.add(Dense(2, activation="softmax")) # Do not change the shape
+model.add(Dense(2)) # Do not change the shape
